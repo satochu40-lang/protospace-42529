79a120f3a75123412032a76e77411e28c040f437
satochu40-lang <satochu.40@gmail.com>
2025-11-11 17:32:03 +0900

    end修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -70,7 +70,8 @@ class PrototypesController < ApplicationController
 
     def move_to_index
       unless user_signed_in?
-      redirect_to action: :index
+        redirect_to action: :index
+      end
     end
   
   private
